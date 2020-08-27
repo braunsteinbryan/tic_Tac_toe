d95469d1c33d8478e9577f905d2bc39c81eee16a
braunsteinbryan <braunsteinbryan@gmail.com>
2020-08-26 20:26:14 -0400

    Added minor details
    - changed apiUrls production key to actual api address
    - created a curl script for signing up
    - edited the name in package-lock.json from GA's to my own

---
+++ curl-scripts/signup.sh
@@ -0,0 +1,15 @@
+# VARIABLE=VALUE sh curl-scripts/signup.sh
+
+curl 'https://tic-tac-toe-api-production.herokuapp.com/sign-up' \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --data '{
+    "credentials": {
+      "email": "'"${EMAIL}"'",
+      "password": "'"${PASSWORD}"'",
+      "password_confirmation": "'"${PASSWORD_CONFIRMATION}"'"
+    }
+  }'
+
+echo
